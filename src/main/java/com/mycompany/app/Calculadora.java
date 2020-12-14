ce3c87766540836cbe537793187d277a6bae12f9
anna-ravaglio <anna-ravaglio@users.noreply.github.com>
2020-12-14 00:53:37 -0300

    Alterando para o código da calculadora

---
+++ src/main/java/com/mycompany/app/Calculadora.java
@@ -0,0 +1,54 @@
+/**
+ * Hello world!
+ */
+/* public class App
+{
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+    
+
+} */
+
+package com.mycompany.app;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static int operacao(int first, String operator, int second) {
+
+        int result;
+
+        switch (operator) {
+        case "+":
+            result = first + second;
+            return result;
+
+        case "-":
+            result = first - second;
+            return result;
+
+        case "*":
+            result = first * second;
+            return result;
+
+        case "/":
+            result = first / second;
+            return result;
+
+        default:
+            System.out.printf("Error! operator is not correct");
+            return 0;
+        }
+    }
+
+}
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -32,12 +32,26 @@ public class AppTest
     @Test
     public void testAppMain()
     {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
+        // App.main(null);
+        // try {
+        //     assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        // } catch (AssertionError e) {
+        //     fail("\"message\" is not \"Hello World!\"");
+        // }
+        int soma, sub, mult, div;
+
+        soma = Calculadora.operacao(1, "+", 2);
+        System.out.printf("Resultado 1 + 2 = ", soma);
+
+        sub = Calculadora.operacao(2, "-", 1);
+        System.out.printf("Resultado 2 - 1 = ", sub);
+
+        mult = Calculadora.operacao(3, "*", 3);
+        System.out.printf("Resultado 3 * 3 = ", mult);
+
+        div = Calculadora.operacao(4, "/", 2);
+        System.out.printf("Resultado 4 / 2 = ", div);
+
     }
 
     @After
